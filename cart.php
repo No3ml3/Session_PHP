9c29df1f266143c7d8456277e87bb0ca770b31a2
Noemie.BARRE <noemie.barre24@gmail.com>
2023-04-18 12:26:54 +0200

    exercice

--- cart.php
+++ cart.php
@@ -1,7 +1,30 @@
 <?php require 'inc/head.php'; ?>
+<?php require 'inc/data/products.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
     </div>
+
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Panier</title>
+    </head>
+
+    <body>
+        <h1>Panier</h1>
+        <ul>
+            <?php
+            foreach (array_count_values($_SESSION['cart']) as $cartKey => $qte) : ?>
+                <li><?= $catalog[$cartKey]['name'] ?> = <?= $qte ?></li>
+            <?php endforeach ?>
+        </ul>
+        <div>
+            <p>total: <?= count($_SESSION['cart']) ?></p>
+        </div>
+    </body>
+
+    </html>
 </section>
 <?php require 'inc/foot.php'; ?>
--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php require 'inc/data/products.php'; ?>
 <?php require 'inc/head.php'; ?>
+<?php
+
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = array();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    if (!empty($_GET)) {
+        $myGet = array_map('trim', $_GET);
+    }
+
+    if (!empty($myGet['add_to_cart'])) {
+        array_push($_SESSION['cart'], $myGet['add_to_cart']);
+        header('location: index.php');
+    }
+}
+
+?>
 <section class="cookies container-fluid">
     <div class="row">
-        <?php foreach ($catalog as $id => $cookie) { ?>
+        <?php foreach ($catalog as $id => $cookie) : ?>
             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <figure class="thumbnail text-center">
                     <img src="assets/img/product-<?= $id; ?>.jpg" alt="<?= $cookie['name']; ?>" class="img-responsive">
@@ -15,7 +34,7 @@
                     </figcaption>
                 </figure>
             </div>
-        <?php } ?>
+        <?php endforeach ?>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
